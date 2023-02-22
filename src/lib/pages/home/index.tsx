304256f86919c1bb51588ed1700c7066f2d28339
highlander <bithighlander@gmail.com>
2023-02-22 17:13:27 -0600

    api key

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <Grid gap={4}>
       <iframe
-        src="https://buy.onramper.com"
+        src="https://buy.onramper.com?apiKey=pk_prod_OQ4CJ7wEyjVocOy3ibd45TUceLIlZHQlpugRV86g6SY0"
         height="540px"
         width="360px"
         title="Onramper widget"
