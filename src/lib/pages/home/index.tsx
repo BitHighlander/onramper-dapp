8075c8cabe94d7a6556bb1066d25718d4b1ac634
highlander <bithighlander@gmail.com>
2023-02-22 17:06:43 -0600

    fix iframe

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,26 +1,17 @@
 import {
   Grid,
-  Tabs,
-  Tab,
-  TabList,
-  TabPanels,
-  TabPanel,
 } from "@chakra-ui/react";
-
-import SomeText from "./components/SomeText";
-import Pubkeys from "./components/Pubkeys";
 
 const Home = () => {
   return (
     <Grid gap={4}>
       <iframe
-          style="border-radius: 4px; border: 1px solid #58585f; margin: auto;max-width: 360px"
-          src="https://buy.onramper.com"
-          height="540px"
-          width="360px"
-          title="Onramper widget"
-          allow="accelerometer; autoplay; camera; gyroscope; payment">
-      </iframe>
+        src="https://buy.onramper.com"
+        height="540px"
+        width="360px"
+        title="Onramper widget"
+        allow="accelerometer; autoplay; camera; gyroscope; payment"
+      />
     </Grid>
   );
 };
